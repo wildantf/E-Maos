1f069a7b29c3ac1761eeffc7a2b58a335631fb2b
muzammilnm <zammils61@gmail.com>
2020-03-21 15:08:26 +0700

    add login,register and sigin or register

---
+++ app/src/main/java/com/dicoding/picodiploma/e_maos/siginOrRegister.java
@@ -0,0 +1,14 @@
+package com.dicoding.picodiploma.e_maos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class siginOrRegister extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sigin_or_register);
+    }
+}
